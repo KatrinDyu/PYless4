8b831f21a8e6eaf7d1b2f89ec8add903ef22e237
Ekaterina <dyuldina32@gmail.com>
2023-07-04 21:57:38 +0500

    1st

---
+++ ex1.py
@@ -0,0 +1,28 @@
+#Даны два неупорядоченных набора целых чисел (может быть, с повторениями). 
+#Выдать без повторений в порядке возрастания все те числа, которые встречаются
+#в обоих наборах.Пользователь вводит 2 числа. n — кол-во элементов первого множества. 
+#m — кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств.
+
+from random import randint
+n = int(input())
+m = int(input())
+arr_n = []   #рандомно вывели список
+for i in range(1, n):
+    arr_n.append(randint(1, 10))
+#print(arr_n)
+my_set_n = set(arr_n)
+#print(my_set_n)
+arr_m = []   
+for i in range(1, m):
+    arr_m.append(randint(1, 10))
+my_set_m = set(arr_m)
+my_set_m = set(arr_m)
+
+print(arr_n)
+print(arr_m)
+print(my_set_n)
+print(my_set_m)
+
+
+cross = my_set_n.intersection(my_set_m)
+print(cross)
